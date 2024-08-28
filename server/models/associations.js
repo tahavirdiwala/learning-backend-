6bd1d6ee4cee0537bcb76c13436cf47bbaabb353
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-08-28 07:50:20 +0530

    give jsdoc for models

--- server/models/associations.js
+++ server/models/associations.js
@@ -1,10 +1,17 @@
 const { Sequelize } = require("sequelize");
 const sequelize = require("../db/sequelize");
 
+/**
+ * @type {import('sequelize').Model}
+ */
 const Categories = require("../models/categories")(
   sequelize,
   Sequelize.DataTypes
 );
+
+/**
+ * @type {import('sequelize').Model}
+ */
 const Products = require("../models/product")(sequelize, Sequelize.DataTypes);
 
 Products.belongsTo(Categories, { foreignKey: "categoryId", as: "category" });
